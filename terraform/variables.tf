d5041c7d63c0ff395c4a4c1e5e33fe5edb921733
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-01 19:54:51 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,11 @@
-variable "region" {
+variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "ami" {
-  default = "ami-04b70fa74e45c3917"
+variable "instance_type" {
+  default = "t2.small"
+}
+
+variable "key_pair" {
+  default = "Veera"
 }

5f8d8b1068afe2d397c48178f627007978a27c1d
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-01 19:23:35 +0530

    Update admin.js

--- strapi-project/config/admin.js
+++ strapi-project/config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','JH/J9xB0XdEij/ig+8ZXCQ=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','a1qAuJoVtucvh+BM8ylHzQ=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT','5HiluKwLjyE/cxrpHgdtnQ=='),
     },
   },
   flags: {

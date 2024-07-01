16b8b735c796b255ab7be55e5cefa4d118fd0c59
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-01 19:21:02 +0530

    Update server.js

--- strapi-project/config/server.js
+++ strapi-project/config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ["2F42N7Pc6c+h8n+YNe7Pmg==,wvjgBZAN4Uzn12USExEwhw==,+nk0yo7aa6nKcp9d6g6rPw==,/da60z2KGfSZU9qqWRoXVA=="],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
